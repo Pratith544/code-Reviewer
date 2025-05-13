f18c56feb71e02e6d1ba9d68ff1b176d95be5d03
Pratith544 <pratithbhat@gmail.com>
2025-05-13 23:52:13 +0530

    Add encoding fallback for node-fetch

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  images:    { unoptimized: true },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
